00cbe86d978c589e894766d1079e4415a360607f
Olga <minaeva9@gmail.com>
2024-04-24 16:51:29 -0400

    input task style

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -5,6 +5,12 @@ export default function TodoList() {
   const [tasks, setTasks] = useState("");
   return (
     <>
+      <h1>TODO LIST</h1>
+      <div className="todo--input">
+        <button className="btn">All Tasks</button>
+        <input type="text" placeholder="Set Task" />
+        <button className="btn">Add Task</button>
+      </div>
       <TodoItem />
     </>
   );
